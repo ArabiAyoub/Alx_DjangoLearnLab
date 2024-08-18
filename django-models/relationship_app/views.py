8285c289d6f9fcb927aff70c8f3c8b62b839e308
ArabiAyoub <ayoub.arabi.98@gmail.com>
2024-08-18 01:50:33 +0100

    add views and hmtl

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+def list_books(request):
+    books = Book.objects.all()
+    content = '<h1>Books Available:</h1><ul>'
+    for book in books:
+        content += f'<li>{book.title} by {book.author.name}</li>'
+    content += '</ul>'
+    return HttpResponse(content)
